fad3171cb9b465ba021823ab2d2a02fcc514ee51
astradeni <astradeni@mail.ru>
2020-11-17 16:09:38 +0100

    CORS Error fix

--- src/main/java/de/astradeni/budget/web/controller/frontend/HomeController.java
+++ src/main/java/de/astradeni/budget/web/controller/frontend/HomeController.java
@@ -1,12 +1,14 @@
 package de.astradeni.budget.web.controller.frontend;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping(value = {"/","/categories"})
+	@CrossOrigin
 	public String index() {
 		return "index";
 	}
