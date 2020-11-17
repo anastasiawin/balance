8dd9524f8f23145ae1c870e1958f1d1fd47f3a58
astradeni <astradeni@mail.ru>
2020-11-17 17:04:18 +0100

    Fix

--- src/main/resources/js/service/ExpensesDataService.jsx
+++ src/main/resources/js/service/ExpensesDataService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BUDGET_API_URL = 'http://budgetwin.herokuapp.com'
+const BUDGET_API_URL = 'https://budgetwin.herokuapp.com'
 const API_URL = `${BUDGET_API_URL}/api`
 
 class ExpensesDataService {
